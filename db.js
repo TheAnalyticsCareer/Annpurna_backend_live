0e0e13f398bd9c47542e3d98a33361ba46f441bb
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-05-29 13:33:26 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
-// db.js
-const { Pool } = require("pg");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectionLimit: 10,
 });
+
+
+
+
 
 module.exports = pool;
